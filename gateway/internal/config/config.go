384a55e2e65f67f545d6f534bbbc1ece875f0d34
Novip1906 <novipcs@gmail.com>
2026-01-02 12:16:44 +0300

    [gateway]: add rate limiter middleware

--- gateway/internal/config/config.go
+++ gateway/internal/config/config.go
@@ -8,17 +8,23 @@ import (
 )
 
 type Config struct {
-	Address      string `yaml:"address" env-default:":8080"`
-	Env          string `yaml:"env" env-default:"dev"`
-	AuthAddress  string `yaml:"auth-address" env-required:"true"`
-	TasksAddress string `yaml:"tasks-address" env-required:"true"`
-	Redis        string `yaml:"redis"`
+	Address      string      `yaml:"address" env-default:":8080"`
+	Env          string      `yaml:"env" env-default:"dev"`
+	AuthAddress  string      `yaml:"auth-address" env-required:"true"`
+	TasksAddress string      `yaml:"tasks-address" env-required:"true"`
+	Redis        Redis       `yaml:"redis"`
+	RateLimiter  RateLimiter `yaml:"rate-limiter" env-required:"true"`
 }
 
 type Redis struct {
 	Address  string `yaml:"address" env-default:":6379"`
 	Password string `yaml:"password" env-default:""`
 	DB       int    `yaml:"db" env-default:"0"`
+}
+
+type RateLimiter struct {
+	RPS   int `yaml:"rps" env-required:"true"`
+	Burst int `yaml:"burst" env-required:"true"`
 }
 
 func MustLoadConfig() *Config {
